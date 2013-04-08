5350dbc64ca5ebf433944f655a76a970284de69d
Michael Helmling <michaelhelmling@posteo.de>
2013-04-08 17:30:38 +0200

    Some more work here

--- exdb_webapp/__init__.py
+++ exdb_webapp/__init__.py
@@ -1,30 +1,70 @@
-from flask import Flask, jsonify, g, render_template, request, url_for
-import subprocess, os
-import exdb
+from flask import Flask, jsonify, g, render_template, redirect, request, session, url_for
+import subprocess, os, shutil
+
+import exdb.tex, exdb.sql
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+from functools import wraps
 
-@app.route('/add/')
-@app.route('/exercise/<id>')
-def hello(id=None):
-    return render_template('add.html', user="ruzika")
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user") is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route("/")
+@login_required
+def list():
+    exercises = exdb.sql.exercises(g.db)
+    return render_template('list.html', exercises=exercises)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        users = {}
+        import hashlib
+        for row in g.db.execute("SELECT * FROM users"):
+            users[row["user"]] = row["password"]
+        user = request.form["username"]
+        if user not in users:
+            error = "Invalid username"
+        elif hashlib.sha256(request.form["password"]).hexdigest() != users[user]:
+            error = "Invalid password"
+        else:
+            session['user'] = user
+            return redirect(url_for("list"))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+@app.route('/add')
+@login_required
+def add(id=None):
+    return render_template('add.html')
 
 @app.route('/rpclatex', methods=["POST", "GET"])
+@login_required
 def rpclatex():
-    texdir = os.path.join(os.path.dirname(__file__), "static", "tex")
-    with open(os.path.join(texdir, "edit.tex"), "wt") as f:
-        f.write(request.form['tex'].encode('utf-8'))
-    with open(os.path.join(texdir, "preamble.tex"), "wt") as f:
-        for line in request.form['preamble']:
-            f.write(line.encode('utf-8') + '\n')
-    try:
-        output = subprocess.check_output(["pdflatex", "-interaction=nonstopmode", "frame.tex"], cwd=texdir, stderr=subprocess.STDOUT)
-        subprocess.check_call(["convert", "-density", "300", "frame.pdf", "frame.png"], cwd=texdir)
-        return jsonify(status="ok", imgsrc=url_for("static", filename="tex/frame.png"))
-    except subprocess.CalledProcessError as e:
-        output = e.output
+    tex = request.form['tex'].encode('utf-8')
+    imgfile = exdb.tex.makePreview(tex)
+    staticPath = os.path.join(app.root_path, "static", "preview.png")
+    shutil.copyfile(imgfile, staticPath)
+    return jsonify(status="ok", imgsrc=url_for("static", filename="preview.png"))
+    
     return jsonify(status="error", log=output)
+
+@app.before_request
+def before_request():
+    g.db = exdb.sql.connect()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
--- runtest.py
+++ runtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-# Copyright 2013 helmling
+# Copyright 2013 Michael Helmling
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -11,5 +11,6 @@ import exdb
 import sys
 
 app.debug = True
-exdb.init(sys.argv[0])
+app.config['SECRET_KEY'] = 'udtiarenudiatrenuditronieadtorn'
+exdb.init(sys.argv[1])
 app.run(host="0.0.0.0",port=6000)
