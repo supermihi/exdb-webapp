0127f84c40a0bdcd43a2d32d9f1f5f4a6b3d1d49
Michael Helmling <michaelhelmling@posteo.de>
2013-05-27 11:48:43 +0200

    Increment version to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup, find_packages
 
 setup(name='exdb-webapp',
-      version='0.2',
+      version='0.2.1',
       description='A flask-driven web application to manage an exdb exercise database',
       author='Michael Helmling',
       author_email='michaelhelmling@posteo.de',
