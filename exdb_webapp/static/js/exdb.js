1e1318cfa679c7b9448d70f042ec1d46b9ffae44
Michael Helmling <michaelhelmling@posteo.de>
2013-06-02 19:16:40 +0200

    More refactoring, commenting and cleanup: removed add.js

--- exdb_webapp/static/js/exdb.js
+++ exdb_webapp/static/js/exdb.js
@@ -1,5 +1,11 @@
-/** enables a jQuery UI tooltip for all elements having an "imgurl" attribute. The tooltip shows
- *  the appropriate image.
+
+/* -------------------------------------------------------------
+ * A. ---------- EXERCISE LIST VIEW RELATED CODE ---------------
+ * -----------------------------------------------------------*/
+
+/** enables a jQuery UI tooltip for all elements having an "imgurl" attribute.
+ * 
+ * The tooltip has CSS class "tooltipimage" and shows the specified image.
  */
 function enableImgTooltips() {
     $(document).tooltip({
@@ -11,7 +17,12 @@ function enableImgTooltips() {
     });
 }
 
-var pageSize = 10; // how many exercises to display per page
+
+/*  ----------- Searching and Navigating  --------------- */
+
+/** number of exercises to display per page; ATM this cannot be changed */
+var pageSize = 10;
+
 
 /** Update the links to directly go to a specific page in the results list.
  * 
@@ -41,6 +52,7 @@ function updatePageButtons(currentPage, totalNumber) {
 	}
 }
 
+
 /** Perform an AJAX search of exercises for the given filters. Requires that an element
  *  #tagtree exists and is a dynatree instance; tag and category filters will be obtained
  *  from this tree.
@@ -51,9 +63,9 @@ function updatePageButtons(currentPage, totalNumber) {
  *  Resulting exercises are placed in the #exerciselist element.
  */
 function searchExercises(page) {
-    var langs = []
-    var tags = []
-    var categories = []
+    var langs = [];
+    var tags = [];
+    var categories = [];
     $.each($("#tagtree").dynatree("getTree").getSelectedNodes(true), function(i, value) {
         if (value.data.is_tag)
             tags.push(value.data.title);
@@ -70,8 +82,7 @@ function searchExercises(page) {
     	pagination["descending"] = true;
     if (!page)
     	page = 0;
-    pagination["offset"] = pageSize*page;
-    	
+    pagination["offset"] = pageSize * page;
     $.post(searchUrl,
            {
                tags: JSON.stringify(tags),
@@ -88,6 +99,21 @@ function searchExercises(page) {
     );
 }
 
+
+/** Uncheck all .filterbutton elements, clear #searchfield, and uncheck everything
+ *  in #tagtree. Then call searchEexercises() again.
+ */
+function clearFilters() {
+    $(".filterbutton").prop("checked", false).button("refresh");
+    $("#searchfield").val("");
+    $("#tagtree").dynatree("getRoot").visit(function(node) {
+        node.select(false);
+    });
+    searchExercises();
+}
+
+
+/*  ---------- Exercise Sorting  --------------- */
 
 /** Initializes the buttons for sorting exercise results by date, author, or description
  *  
@@ -135,19 +161,6 @@ function setSorting(elem, direction) {
 	elem.data("sort", direction);
 }
 
-
-/** Uncheck all .filterbutton elements, clear #searchfield, and uncheck everything
- *  in #tagtree. Then call searchEexercises() again.
- */
-function clearFilters() {
-    $(".filterbutton").prop("checked", false).button("refresh");
-    $("#searchfield").val("");
-    $("#tagtree").dynatree("getRoot").visit(function(node) {
-        node.select(false);
-    });
-    searchExercises();
-}
-
 /** Remove the specified exercise.
  * 
  * First displays a dialog; if that is accepted, the exercise is removed in a synchronous AJAX call.
@@ -168,6 +181,12 @@ function removeExercise(creator, number) {
 	});
 	$("#confirm_dialog").dialog("open");
 }
+
+
+/* -------------------------------------------------------------
+ * B. ---------------- TAG TREE RELATED CODE -------------------
+ * -----------------------------------------------------------*/
+
 
 /** Create a dynatree on the #tagtree element that loads its contents via AJAX from
  *  tagTreeUrl.
@@ -214,7 +233,7 @@ function makeTagTree(dnd) {
 }
 
 
-/** Initialize buttons for constrolling the tag tree (add, remove, rename).
+/** Initialize buttons for modifying the tag tree (add, remove, rename).
  */
 function initTagTreeControls() {
     $("#addcategory").button().click(function(event) {
@@ -286,6 +305,43 @@ function tags(selector) {
 }
 
 
+/* -------------------------------------------------------------
+ * C. --------------- TAG TEXT INPUT FIELD CODE ----------------
+ * -----------------------------------------------------------*/
+
+
+/** Installs autocompletion on the #tags input field.
+ * 
+ * Uses the global variable "availableTags" as information source.
+ */
+function installTagAutocompletion() {
+	// taken from jQueryUI examples
+    $("#tags").bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).data( "ui-autocomplete" ).menu.active ) {
+                event.preventDefault();
+            }
+        }).autocomplete({
+            minLength: 0,
+            source: function( request, response ) {
+                response( $.ui.autocomplete.filter(
+                    availableTags, split(request.term).pop() ) );
+            },
+            focus: function() {
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = split( this.value );
+                terms.pop();
+                terms.push( ui.item.value );
+                terms.push("")
+                this.value = terms.join( ", " );
+                return false;
+            }
+        });
+}
+
+
 /** Split a comma-separated string, ignoring any whitespaces around commas
  */
 function split(val) {
@@ -293,13 +349,6 @@ function split(val) {
     if (ret[0] == "")
         ret = [];
     return ret;
-}
-
-
-/** Return the last element of *val* after being split.
- */ 
-function extractLast(val) {
-    return split(val).pop();
 }
 
 
@@ -320,37 +369,9 @@ function updateTagField(tag) {
 }
 
 
-/** Installs autocompletion on the #tags input field.
- * 
- * Uses the global variable "availableTags" as information source.
- */
-function installTagAutocompletion() {
-	// taken from jQueryUI examples
-    $("#tags").bind( "keydown", function( event ) {
-            if ( event.keyCode === $.ui.keyCode.TAB &&
-                $( this ).data( "ui-autocomplete" ).menu.active ) {
-                event.preventDefault();
-            }
-        }).autocomplete({
-            minLength: 0,
-            source: function( request, response ) {
-                response( $.ui.autocomplete.filter(
-                    availableTags, extractLast( request.term ) ) );
-            },
-            focus: function() {
-                return false;
-            },
-            select: function( event, ui ) {
-                var terms = split( this.value );
-                terms.pop();
-                terms.push( ui.item.value );
-                terms.push("")
-                this.value = terms.join( ", " );
-                return false;
-            }
-        });
-}
-
+/* -------------------------------------------------------------
+ * D. -------------- ADD/EDIT PAGE RELATED CODE ----------------
+ * -----------------------------------------------------------*/
 
 /** Initializes CodeMirror editors on all ".latexeditor" textareas
  * 
@@ -435,6 +456,7 @@ function fillEditFields(exJson) {
 		var typ = $(elem).attr("textype");
 		var lang = $(elem).attr("lang");
 		if (exJson["tex_"+typ] && exJson["tex_"+typ][lang]) {
+			console.log(editors["solutionDE"]);
 			editors[typ+lang].setValue(exJson["tex_"+typ][lang]);
 			$(elem).find('.texpreview').attr("src", exJson["preview_"+typ+lang]);
 		}
@@ -442,7 +464,134 @@ function fillEditFields(exJson) {
 }
 
 
-/* Create and hide the wait_submit dialog */ 
+/* -------------------------------------------------------------
+ * E. ---------------- RPC LaTeX RELATED CODE ------------------
+ * -----------------------------------------------------------*/
+
+
+/** Compile the LaTeX code snippet in the given ".textab" element.
+ * 
+ *  Issues an AJAX call to the rpclatex function and displays the result
+ *  in either the corresponding ".texpreview" or ".errorlog". 
+ */
+function compileSnippet(textab) {
+    var textype = textab.attr("textype");
+    var lang = textab.attr("lang");
+    var editor = editors[textype+lang];
+    if ($.trim(editor.getValue()) == "")
+        return 0;
+    /* create and start an infinite progress dialog */
+    var progress = $('<div class="progressbar"></div>');
+    var proglabel = $('<div class="progress-label">compiling, please be patient...</div>');
+    progress.append(proglabel);
+    progress.progressbar({value: false });
+    textab.prepend(progress);
+    data = {
+    		tex          : editor.getValue(),
+    		lang         : lang,
+    		type         : textype,
+    		tex_preamble : JSON.stringify(preambles())
+    		};
+    if (mode === "edit") {
+        data.creator = creator;
+        data.number = number;
+    }
+    req = $.ajax({
+        type : 'POST',
+        url  : rpclatexUrl,
+        data : data,
+        dataType : 'json',
+        success : function (resp) {
+            if (resp['status'] === 'ok')
+            	setPreviewUrl(type, lang, resp['imgsrc']);
+            else
+            	setErrorLog(type, lang, resp["log"]);
+        },
+        error : function (req, status, err) {
+        	setErrorLog(type, lang, "Server communication error");
+        }
+    });
+    req.always(progress.remove);
+    return req;
+}
+
+
+/** Set a preview image for the specified TeX tab and hide the error log.
+ */
+function setPreviewUrl(type, lang, src) {
+	var textab = $('.textab[textype="'+type+'"][lang="'+lang+'"]');
+	textab.find(".texpreview").attr("src", src).show();
+	textab.find(".errorlog").hide();
+}
+
+
+/** Set an error log message for the specified TeX tab and hide the preview image.
+ */
+function setErrorLog(type, lang, log) {
+	var textab = $('.textab[textype="'+type+'"][lang="'+lang+'"]');
+	textab.find(".errorlog>pre").text(log);
+	textab.find(".errorlog").show();
+	textab.find(".texpreview").hide();
+}
+
+
+/** Submit a new or modified exercise. On success this returns to the "list" page.
+ */ 
+function submit() {
+	$("#wait_submit").dialog("open");
+    var data = {};
+    var tex_solution = {};
+    var tex_exercise = {};
+    $(".textab").each(function(index, elem) {
+        var textype = $(elem).attr("textype");
+        var lang = $(elem).attr("lang");
+        var editor = editors[textype+lang];
+        if ($.trim(editor.getValue()) == "")
+            return;
+        if (textype === "exercise")
+            tex_exercise[lang] = $.trim(editor.getValue());
+        else
+            tex_solution[lang] = $.trim(editor.getValue());
+    });
+    data["tex_exercise"] = tex_exercise;
+    data["tex_solution"] = tex_solution;
+    data["tex_preamble"] = preambles();
+    data["description"] = $("#description").val();
+    data["tags"] = tags("#tags");
+    $.ajax({
+        type : 'POST',
+        url : window.location.pathname,
+        data: { data : JSON.stringify(data) },
+        dataType : 'json',
+        success : function(resp) {
+        	if (resp['status'] === 'ok')
+        		window.location.replace(listUrl);
+        	else {
+        		if (resp['status'] == 'errormsg')
+        			alert(resp['log']);
+        		else {
+        			console.log(resp);
+        			for (var i=0; i < resp['okays'].length; ++i) {
+        				var ok = resp['okays'][i];
+        				setPreviewUrl(ok.type, ok.lang, ok['imgsrc']);
+        			}
+        			setErrorLog(resp['type'], resp['lang'], resp['log']);
+        			alert("Error compiling " + resp['type'] + " " + resp['lang']);
+        		}
+        			
+        	}
+        },
+        error : function(req, status, err) {
+            alert("internal server error while submitting exercise");
+        },
+        async : false
+    }).always(function() {
+    	$("#wait_submit").dialog("close");
+    });
+}
+
+
+/* Create and hide the dialogs which are used in several views */ 
 $(function() {
 	$("#wait_submit").dialog({
         autoOpen:false,
