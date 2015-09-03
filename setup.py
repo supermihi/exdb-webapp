4197ed5ae75a8bbd203c1220378a2322afaf911a
Michael Helmling <michaelhelmling@posteo.de>
2015-09-03 14:45:52 +0200

    Python3 port

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2013 Michael Helmling
+# Copyright 2015 Michael Helmling
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
